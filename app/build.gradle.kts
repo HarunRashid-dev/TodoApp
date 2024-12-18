d6c0b2a443390e0e38a1f92addbdf6012c033677
Admin <harunrashid9159@gmail.com>
2024-12-18 06:42:25 +0530

    code done till 6:42 AM 18/12/2024

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,6 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
@@ -56,4 +55,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.6")
+
 }
--- app/src/main/java/np/com/bimalkafle/todoapp/Todo.kt
+++ app/src/main/java/np/com/bimalkafle/todoapp/Todo.kt
@@ -1,5 +1,6 @@
 package np.com.bimalkafle.todoapp
 
+import java.time.Instant
 import java.util.Date
 
 data class Todo(
@@ -7,3 +8,13 @@ data class Todo(
     var title: String,
     var createdAt : Date
 )
+
+
+fun getFakeTodo() : List<Todo>{
+    return listOf<Todo>(
+        Todo(1,"First todo",Date.from(Instant.now())),
+        Todo(2,"Second todo",Date.from(Instant.now())),
+        Todo(3,"this is my third todo",Date.from(Instant.now())),
+        Todo(4,"this will be my forth todo so that i can use it in UI",Date.from(Instant.now())),
+        );
+}
